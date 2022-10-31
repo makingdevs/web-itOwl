8e661779c9eccfb2f0c040ce018381bf3068e172
Jovany Vergara <jovany@makingdevs.com>
2022-10-31 10:25:02 -0600

    [main] create new fuction of dowland

--- js/contac.js
+++ js/contac.js
@@ -9,6 +9,14 @@ const sendNotification = (name, phone, mensaje, email) =>  {
   window.open(`mailto:ruthamol@itowlus.com?subject=Business IT OWL&body=${encodeURIComponent(message)}`, "_target")
 }
 
+const sendFile = () => {
+  const subFile = `Hi! \n
+  1.- Here you will upload your file that you downloaded, once you have completed it
+  2.- I will send it, to be able to be reviewed by the company`
+
+window.open(`mailto:ruthamol@itowlus.com?subject=Business IT OWL&body=${encodeURIComponent(subFile)}`, "_target")
+}
+
 const submitForm = function(event) {
   event.preventDefault();
   const name= $("#name").val();
@@ -19,7 +27,18 @@ const submitForm = function(event) {
   sendNotification(name, phone, mensaje, email);
 };
 
+const submitFileForm = function(event) {
+  event.preventDefault();
+  document.getElementById("form__submit_file").reset();
+  sendFile();
+};
+
 // your form
 var form = document.getElementById("form__submit");
 // attach event listener
 form.addEventListener("submit", submitForm, true);
+
+// your form
+var form = document.getElementById("form__submit_file");
+// attach event listener
+form.addEventListener("submit", submitFileForm, true);
