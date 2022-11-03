48ab6abc341e7796ac50efa71e9d913ff9f9befd
Jovany Vergara <jovany@makingdevs.com>
2022-11-03 12:27:18 -0600

    [main] add instructions in message in candidates

--- js/candidate.js
+++ js/candidate.js
@@ -4,7 +4,29 @@ const sendCandidate = (name, phone, mensaje, email) =>  {
                         Phone: ${phone}
                         Email: ${email}
                         Message: ${mensaje}
-                        debe enviar el formulario que descargo, siempre y cuando lo haya completado`.replace(/  +/g, '');
+                        debe enviar el formulario que descargo, siempre y cuando lo haya completado
+
+                        1.- Download the file Candidate Questionary Skills, CQS.xlsx. 
+
+2.- Fill up the questionary. 
+
+3.- Save the questionary with your name in this form:  
+
+  b. Name MiddleName FirstLastName SecondLastName CQS.xlsx,  
+
+  a. Example: “Maria Antonieta Galvez Fuentes CQS.xlsx” 
+
+4.- In case you don’t legally use one or more of the parts, omit them.  
+
+  a. Example: “Jose Smith” 
+
+5.- Send it back on the Web Page with your resume save it with the same form,  
+
+ Example: 
+
+  a. “Maria Antonieta Galvez Fuentes Resume.docx” 
+
+  b. “Jose Smith Resume.docx”`.replace(/  +/g, '');
 
   //window.open(`https://api.whatsapp.com/send?phone=525583817789&text=${encodeURIComponent(message)}`, "_target")
   window.open(`mailto:ruthamol@itowlus.com?subject=Business IT OWL&body=${encodeURIComponent(message)}`, "_target")
