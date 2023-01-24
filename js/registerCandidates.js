39d305e9480327de46cb026a950270a034a5e47c
Jovany Vergara <jovany@makingdevs.com>
2023-01-24 12:54:23 -0600

    [1] add buttons and callback

--- js/registerCandidates.js
+++ js/registerCandidates.js
@@ -1,23 +1,41 @@
-import { saveCandidate, getCandidates } from "./collection/candidate.js"
-
-window.addEventListener('DOMContentLoaded', async () => {
-  const querySnapshot = await getCandidates()
-
-  querySnapshot.forEach(doc => {
-    console.log(doc.data())
-  });
-
-})
+import { saveCandidate, getCandidates, onGetCandidates, deleteCandidate } from "./collection/candidate.js"
 
 const candidateForm = document.getElementById("candidate-form")
+const candidatesContainer = document.getElementById("candidate-container")
+
+window.addEventListener('DOMContentLoaded', async () => {
+  onGetCandidates((querySnapshot) => {
+
+    let html = ""
+    querySnapshot.forEach(doc => {
+      const candidates = doc.data()
+      html += `
+        <div>
+          <h3> ${candidates.name}</h3>
+          <p> ${candidates.description}</p>
+          <button class="btn-delete" data-id="${doc.id}" >Deleted</button>
+        </div>
+      `;
+    });
+  
+    candidatesContainer.innerHTML = html;
+    const btnDelete = candidatesContainer.querySelectorAll(".btn-delete")
+
+    btnDelete.forEach(btn => {
+      btn.addEventListener("click", ({target: {dataset}}) => {
+        deleteCandidate(dataset.id)
+      });
+    });
+  });
+});
 
 candidateForm.addEventListener("submit", (e) => {
-  e.preventDefault()
+  e.preventDefault();
 
-  const name = candidateForm["candidate-name"]
-  const description =  candidateForm["candidate-description"]
+  const name = candidateForm["candidate-name"];
+  const description =  candidateForm["candidate-description"];
 
-  saveCandidate(name.value, description.value)
+  saveCandidate(name.value, description.value);
 
-  candidateForm.reset()
+  candidateForm.reset();
 })
