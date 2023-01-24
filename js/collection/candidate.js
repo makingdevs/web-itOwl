a8fc0603f32dbfb2f9333a2721969206436387df
Jovany Vergara <jovany@makingdevs.com>
2023-01-24 09:31:48 -0600

    [1] add getDocs

--- js/collection/candidate.js
+++ js/collection/candidate.js
@@ -1,7 +1,8 @@
 
-import { collection, addDoc } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-firestore.js"
+import { collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-firestore.js"
 import { db } from '../config/firebase.js'
 
-export const saveCandidate = (name, description) => {
+export const saveCandidate = (name, description) => 
   addDoc(collection(db, "candidates"), {name: name, description: description})
-}
+
+export const getCandidates = () => getDocs(collection(db, "candidates"))
