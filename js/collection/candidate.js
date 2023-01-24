385d3ca6b6b9b7213a9bcc4a79e05d227621e0df
Jovany Vergara <jovany@makingdevs.com>
2023-01-24 12:54:05 -0600

    [1] create callback for fuction

--- js/collection/candidate.js
+++ js/collection/candidate.js
@@ -1,8 +1,10 @@
 
-import { collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-firestore.js"
+import { collection, addDoc, getDocs, onSnapshot, } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-firestore.js"
 import { db } from '../config/firebase.js'
 
 export const saveCandidate = (name, description) => 
   addDoc(collection(db, "candidates"), {name: name, description: description})
 
 export const getCandidates = () => getDocs(collection(db, "candidates"))
+
+export const onGetCandidates = (callback) => onSnapshot(collection(db, "candidates"), callback)
