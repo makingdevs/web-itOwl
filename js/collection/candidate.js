7d2facd4131682b7555925a6cd73bd88b6f0f84a
Jovany Vergara <jovany@makingdevs.com>
2023-01-24 13:13:57 -0600

    [1] create export delete for id

--- js/collection/candidate.js
+++ js/collection/candidate.js
@@ -1,6 +1,6 @@
 
-import { collection, addDoc, getDocs, onSnapshot, } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-firestore.js"
-import { db } from '../config/firebase.js'
+import { collection, addDoc, getDocs, onSnapshot, deleteDoc, doc, } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-firestore.js";
+import { db } from '../config/firebase.js';
 
 export const saveCandidate = (name, description) => 
   addDoc(collection(db, "candidates"), {name: name, description: description})
@@ -8,3 +8,5 @@ export const saveCandidate = (name, description) =>
 export const getCandidates = () => getDocs(collection(db, "candidates"))
 
 export const onGetCandidates = (callback) => onSnapshot(collection(db, "candidates"), callback)
+
+export const deleteCandidate = id => deleteDoc(doc(db, "candidates", id))
