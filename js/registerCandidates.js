4fbe4f80c3ca064329b4a875b564a1c6a45afd26
Jovany Vergara <jovany@makingdevs.com>
2023-01-24 23:47:30 -0600

    [1] add new elements and create fuction update

--- js/registerCandidates.js
+++ js/registerCandidates.js
@@ -1,20 +1,24 @@
-import { saveCandidate, onGetCandidates, deleteCandidate, getCandidateEdit } from "./collection/candidate.js"
+import { saveCandidate, onGetCandidates, deleteCandidate, getCandidateEdit, updateCandidate } from "./collection/candidate.js"
 
 const candidateForm = document.getElementById("candidate-form")
 const candidatesContainer = document.getElementById("candidate-container")
 
 let editStatus = false
+let idCandidate = ""
 
 window.addEventListener('DOMContentLoaded', async () => {
   onGetCandidates((querySnapshot) => {
-
     let html = ""
+    
     querySnapshot.forEach(doc => {
       const candidates = doc.data()
       html += `
         <tr>
           <td> ${candidates.name}</td>
+          <td> ${candidates.work}</td>
           <td> ${candidates.description}</td>
+          <td> ${candidates.calendar}</td>
+          <td> ${candidates.post}</td>
           <td>
             <button class="btn-delete btn btn-danger" data-id="${doc.id}" >Deleted</button>
             <button class="btn-edit btn btn-primary" data-id="${doc.id}" >Edit</button>
@@ -40,8 +44,14 @@ window.addEventListener('DOMContentLoaded', async () => {
 
         candidateForm["candidate-name"].value = candidate.name
         candidateForm["candidate-description"].value = candidate.description
+        candidateForm["candidate-work"].value = candidate.work
+        candidateForm["candidate-calendar"].value = candidate.calendar
+        candidateForm["candidate-post"].value = candidate.post
 
         editStatus = true
+        idCandidate = doc.id
+
+        candidateForm["candidate-save"].innerText = "Update"
       })
     })
 
@@ -53,12 +63,23 @@ candidateForm.addEventListener("submit", (e) => {
 
   const name = candidateForm["candidate-name"];
   const description =  candidateForm["candidate-description"];
+  const work =  candidateForm["candidate-work"];
+  const calendar =  candidateForm["candidate-calendar"];
+  const post =  candidateForm["candidate-post"];
 
-  if(editStatus) {
-    console.log("update")
+  if(!editStatus) {
+    saveCandidate(name.value, description.value, work.value, calendar.value, post.value);
   } else{
-    saveCandidate(name.value, description.value);
-    candidateForm.reset();
+    updateCandidate(idCandidate, {
+      name: name.value,
+      description: description.value,
+      work: work.value,
+      calendar: calendar.value,
+      post: post.value
+    });
+
+    editStatus = false;
   }
 
+  candidateForm.reset();
 })
