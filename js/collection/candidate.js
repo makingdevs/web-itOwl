a7bb9513ec181f456684c6558f460047bf9ca85f
Jovany Vergara <jovany@makingdevs.com>
2023-01-24 23:47:52 -0600

    [1] create export update candidate

--- js/collection/candidate.js
+++ js/collection/candidate.js
@@ -1,14 +1,16 @@
 
-import { collection, addDoc, getDocs, onSnapshot, deleteDoc, doc, getDoc, } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-firestore.js";
+import { collection, addDoc, getDocs, onSnapshot, deleteDoc, doc, getDoc, updateDoc } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-firestore.js";
 import { db } from '../config/firebase.js';
 
-export const saveCandidate = (name, description) => 
-  addDoc(collection(db, "candidates"), {name: name, description: description})
+export const saveCandidate = (name, description, work, calendar, post) =>
+  addDoc(collection(db, "candidates"), {name: name, description: description, work: work, calendar: calendar, post: post})
 
 export const getCandidates = () => getDocs(collection(db, "candidates"))
 
 export const onGetCandidates = (callback) => onSnapshot(collection(db, "candidates"), callback)
 
-export const deleteCandidate = id => deleteDoc(doc(db, "candidates", id))
+export const deleteCandidate = (id) => deleteDoc(doc(db, "candidates", id))
 
-export const getCandidateEdit = id => getDoc(doc(db, "candidates", id))
+export const getCandidateEdit = (id) => getDoc(doc(db, "candidates", id))
+
+export const updateCandidate = (id, newFields) => updateDoc(doc(db, "candidates", id), newFields)
