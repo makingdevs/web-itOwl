c54282c187f46741f27a6aa3917e5b43670860ff
Jovany Vergara <jovany@makingdevs.com>
2023-07-26 15:45:53 -0600

    [main] change fuction

--- js/login.js
+++ js/login.js
@@ -1,9 +1,17 @@
 function go(){
+  if (document.form.password.value=='test' && document.form.login.value=='test'){
+    document.form.submit();
+  }
+  else{
+    alert("Please enter correct username and password.");
+  }
+}
 
-  if (document.form.password.value=='test' && document.form.login.value=='test'){ 
-          document.form.submit(); 
-      } 
-      else{ 
-           alert("Please enter correct username and password."); 
-      } 
-  } 
+function loginTeam(){
+  if (document.form.password.value=='Mo724@2023' && document.form.login.value=='Temp072023'){
+    document.form.submit();
+  }
+  else{
+    alert("Please enter correct username and password to access team.");
+  }
+}
--- js/loginTeam.js
+++
@@ -1,9 +0,0 @@
-function loginTeam(){
-
-  if (document.form.password.value=='Mo724@2023' && document.form.login.value=='Temp072023'){
-          document.form.submit();
-      }
-      else{
-           alert("Please enter correct username and password.");
-      }
-  }
