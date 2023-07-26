05a25129469e5e20a4f5a31ab8ba34a015397b38
Jovany Vergara <jovany@makingdevs.com>
2023-07-26 15:35:28 -0600

    [main] create fuction login team

---
+++ js/loginTeam.js
@@ -0,0 +1,9 @@
+function loginTeam(){
+
+  if (document.form.password.value=='Mo724@2023' && document.form.login.value=='Temp072023'){
+          document.form.submit();
+      }
+      else{
+           alert("Please enter correct username and password.");
+      }
+  }
