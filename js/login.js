322cad75d82a1e9f3bd5b0be1918c67339636c37
Jovany Vergara <jovany@makingdevs.com>
2023-07-31 17:55:27 -0600

    [6] test login

--- js/login.js
+++ js/login.js
@@ -15,3 +15,17 @@ function loginTeam(){
     alert("Please enter correct username and password to access team.");
   }
 }
+
+//Submit Button
+function loginTeam2() {
+  let userRef = "test";
+  let passRef = "test";
+
+  let user =  document.getElementById("username").value;
+  let pass = document.getElementById("password").value;
+  if (user == userRef || pass == passRef) {
+    document.form.submit();
+  } else {
+    alert("It's seem you make a mistake...")
+  }
+};
